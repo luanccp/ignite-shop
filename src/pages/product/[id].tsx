f02936babf1ec47894342f967dea46eddc5087bb
Luan Castheloge <luanccp@gmail.com>
2024-02-08 13:41:36 +0000

    feat: creating checkout session from stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -16,11 +16,14 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   };
 }
 
 export default function Product({ product }: ProductProps) {
-  const { query } = useRouter();
+  function handleBuyButton() {
+    console.log(product.defaultPriceId);
+  }
 
   return (
     <ProductContainer>
@@ -34,7 +37,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handleBuyButton}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
@@ -73,6 +76,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: "BRL",
         }).format(price.unit_amount! / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, // 1 hours
