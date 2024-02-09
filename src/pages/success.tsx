a4a7f8fbfa131f43fad8c2c897c9df75ec6a0720
Luan Castheloge <luanccp@gmail.com>
2024-02-09 10:37:09 +0000

    feat: designing the success screen

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,6 +1,20 @@
-import { useRouter } from "next/router";
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
 
 export default function Success() {
-    return <h2>Success</h2>;
-  }
-  
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua{" "}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
+}
